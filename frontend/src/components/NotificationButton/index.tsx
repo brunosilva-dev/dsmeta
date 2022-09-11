aea957e97e3cb23ba07385e325950edc79fb8388
brunosilva-dev <silva.dev@hotmail.com>
2022-09-11 11:18:03 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import icon from '../../assets/img/notification-icon.svg';
 import axios from 'axios';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
+import { toast } from 'react-toastify';
 
 type Props = {
   saleId: number;
@@ -9,7 +10,7 @@ type Props = {
 
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-    console.log("Success! -> ID", id);
+    toast.info("SMS enviado com sucesso!");
   });
 }
 
